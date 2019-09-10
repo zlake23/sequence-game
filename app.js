307f2e946c56f0fda6a35642e02d3280778730e4
Zachary <zlake23@gmail.com>
2019-09-10 09:16:22 -0700

    Adds functions for player turn and win conditions

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const gameBoard = () => Array.from(document.getElementsByClassName('card'));
+const allSame = (arr) => arr.every(cardEl => cardEl.innerText === arr[0].innerText && cardEl.innerText !== '');
 
-function clickCard() {
-    console.log('You clicked a card');
-}
+//function clickCard() {
+//    console.log('You clicked a card');
+//}
+
+const takeTurn = (index, letter) => grid()[index].innerText = letter;
+
+const clickCard = (event) => {
+    takeTurn(boxNumId(event.target), 'x');
+    if(!checkForVictory())
+    opponentTurn();
+};
 
 const cardListeners = () => gameBoard().forEach(cardEl => cardEl.addEventListener('click', clickCard));
 
@@ -14,6 +23,17 @@ function playerTurn() {
 };
 
 function sequenceWin() {
+    let victory = false;
 
+    winningCombos.forEach(combo => {
+        const board = gameboard();
+        const sequence = [board[combo[0]], board[combo[1]], board[combo[2]], board[combo[3]], board[combo[4]]];
+        if(allSame(sequence)) {
+            victory = true;
+            endGame(sequence);
+        }
+    });
+
+    return victory;
 };
 
